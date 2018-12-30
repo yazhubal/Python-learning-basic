894480ae688510bb113710313bb7d9f60206a703
Piotr Szuszkiewicz - Yazhubal <piotr.szuszkiewicz@gmail.com>
2018-12-30 15:22:56 +0100

    Added first file

--- Start.py
+++ Start.py
@@ -1,3 +1,4 @@
+<<<<<<< refs/remotes/origin/master
 
 character_name = "John"
 character_age = "35"
@@ -10,3 +11,15 @@ print ("but didn't like being " + character_age + ".")
 character_age = 45
 
 
+=======
+
+character_name = "John"
+character_age = "35"
+
+print ("The once was a man named " + character_name + ",")
+print ("He was " + character_age + " years old.")
+print ("He really liked the name " + character_name+ ",")
+print ("but didn't like being " + character_age + ".")
+
+
+>>>>>>> Added first file

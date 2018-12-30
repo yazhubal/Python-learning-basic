b183e6792399d2f4a90efa6fd9a044c6f56859be
Piotr Szuszkiewicz - Yazhubal <piotr.szuszkiewicz@gmail.com>
2018-12-30 15:22:56 +0100

    Added first file

---
+++ Start.py
@@ -0,0 +1,10 @@
+
+character_name = "John"
+character_age = "35"
+
+print ("The once was a man named " + character_name + ",")
+print ("He was " + character_age + " years old.")
+print ("He really liked the name " + character_name+ ",")
+print ("but didn't like being " + character_age + ".")
+
+

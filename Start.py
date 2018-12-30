eefc5f8904e85efa0fcd33807d7bed9bb0d97a6a
Piotr Szuszkiewicz - Yazhubal <piotr.szuszkiewicz@gmail.com>
2018-12-30 16:14:41 +0100

    Adding new code lines

--- Start.py
+++ Start.py
@@ -7,4 +7,6 @@ print ("He was " + character_age + " years old.")
 print ("He really liked the name " + character_name+ ",")
 print ("but didn't like being " + character_age + ".")
 
+character_age = 45
+
 

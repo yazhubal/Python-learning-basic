83738666962b33f988cd69f90856d20ec6b68733
Piotr Szuszkiewicz - Yazhubal <piotr.szuszkiewicz@gmail.com>
2019-01-05 18:20:11 +0100

    Fixing

---
+++ List.py
@@ -0,0 +1,3 @@
+#lista znajomych bez piewszej osoby od 01 pozycji listy, lista zaczyna się od pozycji 0
+friends = ["Monika", "Piotrek", "Tomek", "Tomek.P", "Justyna", "Rafał", "Witek", "Agnieszka", "Sabina"]
+print(friends [1:])

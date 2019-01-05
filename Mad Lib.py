72120abae7679a889640be9a718d4c741edd7044
Piotr Szuszkiewicz - Yazhubal <piotr.szuszkiewicz@gmail.com>
2019-01-05 18:00:06 +0100

    New code lines

--- Mad Lib.py
+++ Mad Lib.py
@@ -1,7 +1,7 @@
 
 color = input("Enter a color: ")
 plural_noun = input("Enter a plural noun: ")
-color = input("Enter a color: ")
-print("Roses are {color}")
-print("{plurar_noun} are blue")
-print("I love you {celebrity}")
+celebrity = input("Enter a celebrity name: ")
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love you " + celebrity)

4ce59d8870f7e0af5f3747b3439f6786a971be2a
Piotr Szuszkiewicz - Yazhubal <piotr.szuszkiewicz@gmail.com>
2019-09-24 22:58:05 +0200

    New files Hello and list

--- List.py
+++ List.py
@@ -1,3 +1,10 @@
 #lista znajomych bez piewszej osoby od 01 pozycji listy, lista zaczyna się od pozycji 0
 friends = ["Monika", "Piotrek", "Tomek", "Tomek.P", "Justyna", "Rafał", "Witek", "Agnieszka", "Sabina"]
+
+#Dodawanie do listy pozycja, nazwa
+friends.insert(1, "Marek")
+
+#Usuwanie pozycji z listy
+friends.remove("Marek")
+
 print(friends [1:])

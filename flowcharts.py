2d6d9981869052bc5784a93ecb2cdb59370b8987
Piotr Szuszkiewicz - Yazhubal <yazhubal@users.noreply.github.com>
2019-09-25 14:49:37 +0200

    simple code changes

---
+++ flowcharts.py
@@ -0,0 +1,24 @@
+#Flow charts and basics Flow Control
+
+# 1. Bolean Operators
+# and, or , not
+echo = True
+bravo = False
+
+# 1.a Compairson Operators
+# == Equal to
+# != Not equal to
+# < Less than
+# > Greather than
+# <= Less than or equal to
+# >= Greather than or equal to
+
+# 1.b The and Operator's Truth Table
+# True and True     True
+# True and False    False
+# False and True    False
+# False and False   False
+
+
+
+
---
+++ ifelse.py
@@ -0,0 +1,6 @@
+# Flow control If else
+
+name = 'Bob'
+if name == 'Alice':
+    print ('Hi Alice')
+print ('Done')
